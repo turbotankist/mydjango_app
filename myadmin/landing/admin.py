8ebf0e8f7de56695687eb83be0983fb9130ce367
turbotankist <itex@turbotankist.com>
2018-11-03 15:11:36 +0300

    solo remove

--- myadmin/landing/admin.py
+++ myadmin/landing/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from solo.admin import SingletonModelAdmin
+# from solo.admin import SingletonModelAdmin
 
 # from .models import Landpage
 
--- myadmin/landing/models.py
+++ myadmin/landing/models.py
@@ -1,30 +1,30 @@
 from django.db import models
 from django.contrib import admin
-from solo.models import SingletonModel
+# from solo.models import SingletonModel
 
 
-class Landpage(SingletonModel):
-    about = models.TextField(default='')
-    title = models.CharField(max_length=200,default='')
-    phone = models.CharField(max_length=15,default='')
-    address = models.TextField(default='')
-    vk = models.CharField(max_length=30,default='')
-    fb = models.CharField(max_length=30,default='')
-    copyrights = models.TextField(default='')
-    # news = 
+# class Landpage(SingletonModel):
+#     about = models.TextField(default='')
+#     title = models.CharField(max_length=200,default='')
+#     phone = models.CharField(max_length=15,default='')
+#     address = models.TextField(default='')
+#     vk = models.CharField(max_length=30,default='')
+#     fb = models.CharField(max_length=30,default='')
+#     copyrights = models.TextField(default='')
+#     # news = 
 
-    # def save(self):
-    #     self.changed = timezone.now()
-    #     self.save()
+#     # def save(self):
+#     #     self.changed = timezone.now()
+#     #     self.save()
 
-    # def __str__(self):
-    #     return self.title
-    def __unicode__(self):
-        return u"Site Configuration"
+#     # def __str__(self):
+#     #     return self.title
+#     def __unicode__(self):
+#         return u"Site Configuration"
 
-    class Meta:
-        verbose_name = "Landpage"
-        verbose_name_plural = "Landpage"
+#     class Meta:
+#         verbose_name = "Landpage"
+#         verbose_name_plural = "Landpage"
 
 class LandpageAdmin(admin.ModelAdmin):
     fieldsets = (
